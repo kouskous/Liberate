17c8b76bcfd92a1998046454d9168eaa77aa4bb6
Florian <fbautry@gmail.fr>
2016-10-21 16:53:03 +0200

    Close Branch

--- src/main/java/controllers/PrincipalController.java
+++ src/main/java/controllers/PrincipalController.java
@@ -5,12 +5,20 @@
  */
 package controllers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import dao.FichierUserDao;
+import dao.UserDao;
+import models.FichiersUsers;
 import models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 /**
  *
@@ -19,14 +27,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PrincipalController {
     @RequestMapping(value="/", method = RequestMethod.GET)
-    public String index(HttpServletRequest request){
-                // On vérifie qu'une session n'est pas déjà ouverte
-        HttpSession session= request.getSession();
-        User user = (User)session.getAttribute("user");
- 
-        if(user == null) // Pas de session ouverte
-            return "redirect:/login";
-        else // Une session déjà ouverte
-            return "principal";
+    public String index(HttpServletRequest request) {
+        // On vérifie qu'une session n'est pas déjà ouverte
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        EntityManager em = Persistence.createEntityManagerFactory("persistenceUnitLiber8").createEntityManager();
+        FichierUserDao fichierUserDao = new FichierUserDao(em);
+        Map<String, Boolean> arborescence;
+
+        if (user == null) // Pas de session ouverte
+            return "login";
+         // Une session déjà ouverte
+
+
+             arborescence = fichierUserDao.getArborescence(user);
+
+            for (Map.Entry<String, Boolean> fichier : arborescence.entrySet()) {
+                if (fichier.getValue().booleanValue() == true)
+                    System.out.print("Fichier : ");
+                else
+                    System.out.print("Dossier : ");
+                System.out.println(fichier.getKey());
+            }
+
+        return "redirect:/";
     }
 }
