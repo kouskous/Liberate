15ca4c5b40db296413c9c00af578bbc7bc31774e
p1309918 <devnull@localhost>
2016-11-17 14:57:33 +0100

    add file dans l'arbre

--- src/main/webapp/META-INF/resources/liber8/js/newFile.js
+++ src/main/webapp/META-INF/resources/liber8/js/newFile.js
@@ -7,16 +7,33 @@
 $( document ).ready(function() {
    $("#btn_new_file").click(function(){
       filename = $("#input_name_file").val();
-      $.ajax({ 
-        url      : "/Liber8/newFile",
-        dataType : "json",
-        type     : "POST",
-        data     :{
-                    pathFichier: (App.currentVoletElement+'/'+filename).slice(4)
-                  },
-        success  : function(data) {  
-                        
-                    }       
-        });
+      path = App.currentVoletElement.replace(/\//g,'-');
+      path = path.replace('.','__');
+      element = $("#"+path);
+      if ((path != "")&&(!$(element).hasClass("isFile"))){
+        Logicpath = (App.currentVoletElement+"/"+filename).slice(4);
+        $.ajax({ 
+           url      : "/Liber8/newFile",
+           dataType : "json",
+           type     : "POST",
+           data     :{
+                       pathFichier: Logicpath
+                     },
+           success  : function(data) {  
+                            $(".close").trigger("click");
+                            var nodes = App.tree.getAllNodes();
+                            var sourceNode = {};
+                            sourceNode.text = filename;
+                            sourceNode.id = path+"-"+filename;
+                            sourceNode.isFolder = false;
+                            App.tree.addNode(sourceNode, path);
+                            App.tree.rebuildTree();
+                            $("#"+sourceNode.id).addClass("isFile");
+                            $("#"+sourceNode.id).addClass("branche-arbre");
+                            defineArbreEvents();
+                       }       
+           });
+      }
+
    });
 });
