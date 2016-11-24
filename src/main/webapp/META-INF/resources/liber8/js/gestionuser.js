29be3e09c7feaaf0ab386bd8e1eb46c80d629bb5
faddisofiaa : macair <faddisofiaa : macair@wu-pc-03300>
2016-11-24 10:12:24 +0100

    correction gestion users

--- src/main/webapp/META-INF/resources/liber8/js/gestionuser.js
+++ src/main/webapp/META-INF/resources/liber8/js/gestionuser.js
@@ -30,10 +30,14 @@
                 dataType : "json",
                 type     : "GET",
                 success  : function(data) {  
+                    console.log(data);
                    
                     if(data.response ==="true") {
                         var contenu = JSON.parse(data.content); 
-                     
+                     $(".userproject").append('<li><div class="control-group">\n\
+                                        <label class="control-label userline" for="selectError'+i+'">'+contenu[0].utilisateur+'</label>\n\
+                                        <div class="controls userline"><input type ="text" value="'+contenu[0].droit+'" disabled></div></div>'); //add input box
+                                        
                         for (i =0;i<Object.keys(contenu).length-1; i++) 
                             {
                                 if(contenu[i+1].droit==='admin')
@@ -58,7 +62,7 @@
                                        
                                        
                                         $(".userproject").append('<li><div class="control-group">\n\
-                                        <label class="control-label userline" for="selectError'+i+'">'+contenu[i+1].utilisateur+'</label>\n\
+                                        <label class="control-label userline" for="selectError'+i+1+'">'+contenu[i+1].utilisateur+'</label>\n\
                                         <div class="controls userline"><select id="selectError'+i+1+'" data-rel="chosen" name="droit'+i+1+'" class="droit">\n\
                                         <option value="'+contenu[i+1].droit+'">'+contenu[i+1].droit+'</option>\n\
                                         <option value="'+option1+'">'+option1+'</option><option value="'+option2+'">'+option2+'</option></select><a class="remove_field"><input type="hidden" value="'+contenu[i+1].utilisateur+'" class=" add utilisateur'+i+1+'" name="utilisateur'+i+1+'"><i class="icon-trash"></i></a></div></div>'); //add input box
