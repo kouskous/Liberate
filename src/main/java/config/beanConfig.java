2bb739be9bec8ff64707b88f2dc1fed87bb4fc04
luc di sanza <devnull@localhost>
2016-12-08 22:24:21 +0100

    FINAL

--- src/main/java/config/beanConfig.java
+++ src/main/java/config/beanConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package config;
+package config; 
 
 import dao.FichierUserDao;
 import dao.FichiersVersionDao;
